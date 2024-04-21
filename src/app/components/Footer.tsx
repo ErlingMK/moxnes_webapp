1df508f9347c4ca7b201416c4ac8104ac32f01e3
Erling Moxnes Kristiansen <erling.kristiansen@drift.oslo.kommune.no>
2024-04-21 11:24:25 +0200

    Updated footer layout

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,23 +1,27 @@
 export default function Footer() {
   return (
-    <div className="flex flex-col bg-main-blue text-white p-5">
-      <p className={"font-bold"}>Moxnes Jus & Coaching</p>
-      <p className={"text-sm"}>Organisasjonsnummer: 929450035</p>
+    <div className="flex justify-between bg-main-blue text-white p-5">
+      <div className="flex flex-col ">
+        <p className={"font-bold"}>Moxnes Jus & Coaching</p>
+        <p className={"text-sm"}>Organisasjonsnummer: 929450035</p>
+      </div>
 
-      <p className={"mt-2 text-sm"}>Postboks 50, 3120 Teie Nøtterøy</p>
-      <p className={"text-sm"}>
-        <span>E-post:</span>
-        <a className="ml-1 underline " href={"mailto:post@moxnesjus.no"}>
-          post@moxnesjus.no
-        </a>
-      </p>
+      <div className="flex flex-col">
+        <p className={"text-sm"}>Postboks 50, 3120 Teie Nøtterøy</p>
+        <p className={"text-sm"}>
+          <span>E-post:</span>
+          <a className="ml-1 underline " href={"mailto:post@moxnesjus.no"}>
+            post@moxnesjus.no
+          </a>
+        </p>
 
-      <p className="text-sm">
-        <span>Telefon:</span>
-        <a className="ml-1 underline" href={"tel:91379998"}>
-          +47 913 79 998
-        </a>
-      </p>
+        <p className="text-sm">
+          <span>Telefon:</span>
+          <a className="ml-1 underline" href={"tel:91379998"}>
+            +47 913 79 998
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
