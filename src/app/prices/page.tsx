9e75b0e6a081dbff172d2554dc8f20797bac6f64
Erling Moxnes Kristiansen <erling.m.kristiansen@gmail.com>
2024-06-13 18:41:36 +0200

    Update page.tsx

--- src/app/prices/page.tsx
+++ src/app/prices/page.tsx
@@ -3,14 +3,8 @@ export default function Prices() {
     <div className="flex flex-col gap-5 p-5 md:bg-white md:text-black bg-main-blue-faded text-white">
       <h2 className={"text-lg font-semibold"}>Priser</h2>
       <p>
-        Pris for opprettelse av fremtidsfullmakt kr. 1000,- per time pluss moms.
+        Pris for opprettelse av fremtidsfullmakt kr. 1200,- per time eksklusive moms.
         Det vil bli fakturert per påbegynte halve time.
-      </p>
-      <p>
-        Det kan avtales en fast pris om du ønsker det. Prisen vil da fastsettes
-        ut fra antatt bruk av timer. Det vil inngås en oppdragsavtale som
-        inneholder pris, antatt tidsbruk og tidspunkt for ferdigstillelse av
-        oppdraget.
       </p>
     </div>
   );
